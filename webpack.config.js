3c204c6abdacdbffe45f40e52c4065f53b30e97d
jasmin3ashl3y <jasmine@ashleyit.com>
2022-01-14 19:13:07 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 
 const config = {
