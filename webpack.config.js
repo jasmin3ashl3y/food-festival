83054455b77974e7e34153305593046ff53ad602
jasmin3ashl3y <jasmine@ashleyit.com>
2022-01-15 14:56:05 -0500

    trying to install PWA but having trouble with this lesson

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 const config = {
     entry: {
@@ -42,16 +43,31 @@ const config = {
           }),
           new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+          }),
+          new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
           })
     ],
     mode: 'development',
-     devServer: {
-       static: {
-         directory: path.join(__dirname, './'),
-       },
-       compress: true,
-       port: 8080,
-     },
+      devServer: {
+      static: {
+        directory: path.join(__dirname, './'),
+      },
+        compress: true,
+        port: 8080,
+      },
 };
 
 module.exports = config;
